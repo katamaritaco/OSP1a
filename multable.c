3f15c659572d6b158f3a85fce75ea44772ccd7bf
Nicholas Heindl <heindl@macaroni-05.cs.wisc.edu>
2015-02-06 18:43:11 -0600

    print works. Need argument support.

--- multable.c
+++ multable.c
@@ -5,53 +5,22 @@
 
 int main(int argc, char * argv[])
 {
-    int i;
-    printf("kek\n");
-/*
-    int kekTable[100];
+    int *p_mulTable, i, j;
 
-    kekTable[0] = 5;
+    p_mulTable = (int *)malloc(sizeof(int) * 100);//deal with args still
 
-    printf("num: %d\n", kekTable[0]);
+    int * p_tableIterator = p_mulTable;
 
-
-
-    int * p_mulTable;
-
-    p_mulTable = (int *) malloc (sizeof(int) * 100); // allocate 100 ints worsecase
-
-    int * p_ptr = p_mulTable;
-    
-    for (i = 0; i < 10; i++) {
-        *p_ptr = 0;
-        p_ptr++;
-        printf("In the table: %d\n", p_ptr);
+    for(i = 1; i <= 10; i++)//still assumption with args
+    {
+        for(j = 1; j <= 10; j++) 
+        {
+            * p_tableIterator = j*i;
+            printf("%d\t", j * i); 
+            p_tableIterator++;            
+        }
+        printf("\n");
     }
-*/
-
-
-    int *d_array;
-    d_array = (int *)malloc(sizeof(int) * 100);//100 for now 10x10
-    
-    
-    int * ptrToAccess2d = d_array;
-
-
-    for(i = 0; i < 10; i++){//10 to test
-        * ptrToAccess2d = i;
-        printf("numnum: %d\n", *ptrToAccess2d);
-        ptrToAccess2d++;
-
-    }
-
-    int * ptrToAccess2dNew = d_array;
-    for(i = 0; i < 10; i++){//10 to test
-        printf("numnum2: %d\n", *ptrToAccess2dNew+i);
-    }              
-
-
-//    free(p_mulTable);
-    free(d_array);
 
     return 0;
 }
