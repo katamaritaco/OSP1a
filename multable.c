42ae062e50f86783b721fa915e5106eff6009d02
Nicholas Heindl <heindl@macaroni-05.cs.wisc.edu>
2015-02-06 19:32:45 -0600

    added arg support. need to test

--- multable.c
+++ multable.c
@@ -3,17 +3,29 @@
 #include <string.h>
 
 
-int main(int argc, char * argv[])
+int main(int argc, char ** argv)
 {
     int *p_mulTable, i, j;
 
-    p_mulTable = (int *)malloc(sizeof(int) * 100);//deal with args still
+    int arraySize = 10;
+
+    //prevents the segfault caused by accessing nonexistent arg
+    if( argc > 1 )
+    {
+        //a little wasteful with the atoi calls, but oh well... 
+        if( atoi(*(argv + 1)) < 10 && atoi(*(argv + 1)) > 0 )
+        {
+            arraySize = atoi(*(argv + 1));
+        }
+    }
+    
+    p_mulTable = (int *)malloc(sizeof(int) * (arraySize*arraySize));
 
     int * p_tableIterator = p_mulTable;
 
-    for(i = 1; i <= 10; i++)//still assumption with args
+    for(i = 1; i <= arraySize; i++)
     {
-        for(j = 1; j <= 10; j++) 
+        for(j = 1; j <= arraySize; j++) 
         {
             * p_tableIterator = j*i;
             printf("%d\t", j * i); 
