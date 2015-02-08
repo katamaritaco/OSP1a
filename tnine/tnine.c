ce3b24ad22c65c3e875a8909014d5a2da6e19a1c
Nicholas Heindl <heindl@macaroni-04.cs.wisc.edu>
2015-02-07 21:05:40 -0600

    edited makefile to compile all. Not sure if correct...

--- tnine/tnine.c
+++ tnine/tnine.c
@@ -1,11 +1,11 @@
-nclude <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "p1a2.h"
 
 /* This is an array of 10 POINTERS to KeyboardElements
- * */
+ */
 
 KEPtr keypad[10];
 char * letters[10] = { "", "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ", "" };
@@ -20,6 +20,14 @@ int main(int argc, char * argv[])
 		}
 
 // YOUR CODE HERE
+    int i;
+
+    //iterate through all arguments
+    for (i = 0; i < argc - 1; i++)
+    {
+        printf("letters: %s\n", *(letters + i)); //iterate through the array for the lulz
+    }
+    
  	return 0;
 }
 
