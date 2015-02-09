ffdd862166db00c3d700dde111ba2a7ef2ce603e
Nicholas Heindl <heindl@adelie-06.cs.wisc.edu>
2015-02-08 18:58:18 -0600

    adding iterator through args. need bounds check still

--- tnine/tnine.c
+++ tnine/tnine.c
@@ -58,10 +58,23 @@ int main(int argc, char * argv[])
         printf("str kek: %s\n", argumentString);
 
         //go through all letters in the arg
+        //assumption now that we will always have even num or chars in arg
+        int numberTapped, timesTapped;
         for(j = 0; j < strlen(argumentString); j++)
         {
             //printf("letter: %c\n", argumentString[j]);
-
+            
+            if(j % 2 == 0)//check if first char in a series
+            {
+                numberTapped = argumentString[j] - '0';//convert to ascii, sub offset
+                printf("number tapped: %d\n", numberTapped);
+            }
+            else if(j % 2 == 1)
+            {
+                //need bounds checking on both still...
+                timesTapped = argumentString[j] - '0';//converts to ascii, then sub offset
+                printf("times tapped: %d\n", timesTapped);
+            }
         }
     }
 
